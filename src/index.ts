908e3707368bc412fdf8aebd504d266e0eeb9127
Valentín Vera <valentinvera2805@gmail.com>
2026-01-12 20:20:50 -0300

    fix: add urlencoded body parser for Twilio webhook

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 3000
 // Middleware
 app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // Routes
 app.use("/products", productsRouter)
